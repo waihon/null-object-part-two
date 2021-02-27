59903f8f89822bafcd87343bd599845ce27c19bb
Waihon Yew <yewwaihon@gmail.com>
2021-02-27 04:08:36 +0000

    NoSubscription#has_mentoring? is false

--- lib/no_subscription.rb
+++ lib/no_subscription.rb
@@ -1,4 +1,8 @@
 class NoSubscription
   def charge(credit_card)
   end
+
+  def has_mentoring?
+    false
+  end
 end
--- spec/no_subscription_spec.rb
+++ spec/no_subscription_spec.rb
@@ -12,4 +12,12 @@ describe NoSubscription do
       expect(credit_card).not_to have_received(:charge)
     end
   end
+
+  describe "#has_mentoring?" do
+    it "is falsey" do
+      subscription = NoSubscription.new
+
+      expect(subscription.has_mentoring?).to be false
+    end
+  end
 end
